6ee0ffbc0a9769635d1750cb1bc374652c2bc070
cristian <cristian.morariu@sdg.ro>
2025-03-18 16:17:40 +0200

    create task resource

--- backend/app/Http/Controllers/Api/TaskController.php
+++ backend/app/Http/Controllers/Api/TaskController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(){
-        return Task::all();
+        // return Task::all();
+        return TaskResource::collection(Task::all());
+    }
+
+    public function show(Task $task){
+        return new TaskResource($task);
     }
 }
