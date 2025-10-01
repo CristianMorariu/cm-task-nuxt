5c4237be125547030abb57b517e4828b6948264b
Cristian <vergilmorariu@gmmail.com>
2025-10-01 21:56:07 +0300

    corso fix

--- frontend/axios.js
+++ frontend/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-axios.defaults.withCredentials = true;
-axios.defaults.withXSRFToken = true;
+// axios.defaults.withCredentials = true;
+// axios.defaults.withXSRFToken = true;
 
 console.log(axios.defaults.baseURL);
 
